3cd98419fa5eea54241f7dc5c8ee5854010c12cf
Chong S. Lee <chonglee30@gmail.com>
2025-11-18 11:46:16 -0800

    feat: complete checkout process test with 2 versions

--- cypress/e2e/cartTest.cy.js
+++ cypress/e2e/cartTest.cy.js
@@ -14,7 +14,7 @@ if (!standardUser) {
   throw new Error('Missing the standard user')
 }
 
-describe('Product Inventory Item Test', () => {
+describe('Product Inventory Item Checkout Test', () => {
   context('Viewing Cart Checkout Items', { testIsolation: true }, () => {
     beforeEach(() => {
       LoginPage.sessionLogin(sessionId, standardUser.username, standardUser.password)
@@ -90,7 +90,7 @@ describe('Product Inventory Item Test', () => {
         .should('deep.equal', selectedIds);
     })
 
-    it('Test Cart Page Items - Version3 - existing source code', () => {
+    it('Test Cart Page Set Items using localstorage - Ver3 - existing source code', () => {
       const items = [
         'Sauce Labs Bike Light',
         'Sauce Labs Bolt T-Shirt',
@@ -98,15 +98,48 @@ describe('Product Inventory Item Test', () => {
       ]
 
       const selectedIds = items.map((name) => _.find(InventoryData, { name })?.id)
-      items.forEach(InventoryPage.addInventoryItemToCart)
-      InventoryPage.getCartBadge().should('have.text', items.length)
-        .scrollIntoView().click()
+      window.localStorage.setItem('cart-contents', JSON.stringify(selectedIds))
+
+      cy.window().its('localStorage')
+        .invoke('getItem', 'cart-contents')
+        .should('exist') // since we are not sure when application set the item, good to ensure application to set item.
+        .then(JSON.parse)
+        .should('deep.equal', selectedIds);
+
+      cy.get('.shopping_cart_link').click()
 
       cy.location('pathname').should('equal', "/cart.html");
       cy.get('.title').should('have.text', 'Your Cart')
 
       items.forEach((item, index) => {
+        cy.get('.cart_list .cart_item')
+          .should('have.length', 3)
+          .eq(index)
+          .within(() => {
+            cy.contains('.cart_quantity', 1)
+            cy.contains('.inventory_item_name', item)
+          })
+      })
+    })
 
+    it('Test Complete Checkout Process - Detail (My) Version', () => {
+      const items = [
+        'Sauce Labs Bike Light',
+        'Sauce Labs Bolt T-Shirt',
+        'Sauce Labs Onesie',
+      ]
+
+      const selectedIds = items.map((name) => _.find(InventoryData, { name })?.id)
+      window.localStorage.setItem('cart-contents', JSON.stringify(selectedIds))
+      cy.window().its('localStorage')
+        .invoke('getItem', 'cart-contents')
+        .should('exist') // since we are not sure when application set the item, good to ensure application to set item.
+        .then(JSON.parse)
+        .should('deep.equal', selectedIds);
+
+      cy.get('.shopping_cart_link').click()
+      cy.location('pathname').should('equal', "/cart.html");
+      items.forEach((item, index) => {
         cy.get('.cart_list .cart_item')
           .should('have.length', 3)
           .eq(index)
@@ -116,11 +149,61 @@ describe('Product Inventory Item Test', () => {
           })
       })
 
+      cy.get('[data-test="checkout"]').click()  // Click checkout button 
+      cy.location('pathname').should('equal', "/checkout-step-one.html");
+      cy.get('[data-test="firstName"]').type('Michael').should('have.value', 'Michael')
+      cy.get('[data-test="lastName"]').type('Jordan').should('have.value', 'Jordan')
+      cy.get('[data-test="postalCode"]').type('90210').should('have.value', '90210')
+      cy.get('[data-test="continue"]').click()
+
+      cy.location('pathname').should('equal', "/checkout-step-two.html");
+      items.forEach((item, index) => {
+        cy.get('.cart_list .cart_item')
+          .should('have.length', 3)
+          .eq(index)
+          .within(() => {
+            cy.contains('.cart_quantity', 1)
+            cy.contains('.inventory_item_name', item)
+          })
+      })
+      cy.get('[data-test="finish"]').click()
+
+      cy.location('pathname').should('equal', "/checkout-complete.html");
+      cy.get('h2.complete-header').should('have.text', 'THANK YOU FOR YOUR ORDER')
+      cy.get('#checkout_complete_container').within(() => {
+        cy.get('img').should('have.attr', 'alt').and('include', 'Pony Express')
+      })
+
       cy.window().its('localStorage')
         .invoke('getItem', 'cart-contents')
-        .should('exist') // since we are not sure when application set the item, good to ensure application to set item.
-        .then(JSON.parse)
-        .should('deep.equal', selectedIds);
+        .should('not.exist')
     })
+
+    it('Test Complete Checkout Process - concise version with all Ids', () => {
+      // Grab the id property from each item in the InventoryData array
+      const ids = _.map(InventoryData, 'id')
+      window.localStorage.setItem('cart-contents', JSON.stringify(ids))
+
+      cy.visit('/cart.html')
+      cy.get('.cart_list .cart_item').should('have.length', InventoryData.length)
+      cy.contains('button', 'Checkout').click()
+      cy.location('pathname').should('equal', "/checkout-step-one.html");
+      cy.get('.checkout_info_wrapper').within(() => {
+        cy.get('[data-test="firstName"]').type('Michael').should('have.value', 'Michael')
+        cy.get('[data-test="lastName"]').type('Jordan').should('have.value', 'Jordan')
+        cy.get('[data-test="postalCode"]').type('90210').should('have.value', '90210')
+        cy.get('input[type=submit]').should('have.attr', 'value', 'Continue').click()
+      })
+      cy.location('pathname').should('equal', "/checkout-step-two.html");
+      cy.get('.cart_list .cart_item').should('have.length', InventoryData.length)
+      cy.contains('[data-test=finish]', 'Finish').click()
+      cy.location('pathname').should('equal', "/checkout-complete.html");
+
+      cy.get('#checkout_complete_container').should('be.visible')
+      cy.window().its('localStorage')
+        .invoke('getItem', 'cart-contents')
+        .should('not.exist')
+    })
+
   })
 })
